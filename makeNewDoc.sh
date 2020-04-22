62858445c1289d676a4bf4f5734b994ccbe76e66
ShowPenZ <showpenz@tutanota.com>
2020-04-22 17:56:55 +0800

    update

--- makeNewDoc.sh
+++ makeNewDoc.sh
@@ -4,20 +4,21 @@
 set -e
 
 lang=$1
+
+dirname=$2
+
+filename=$3
 # 进入文件夹
 
 cd docs${lang}/javascript
 
-# 判断目录是不是已经存在，如果不存在则创建，存在则输出“dir exist”
-dirname=$2
-
-filename=$3
+pwd
 
 echo "the dir name is $dirname"
 
 #    判断文件是否存在
-if [ ! -f $filename ]; then
-    touch ${dirname}/${filename}.md
+if [[ ! -f ${dirname}/"$filename.md" ]]; then
+    touch ${dirname}/"$filename".md
 else
     echo file exist, please make a new file
 fi
